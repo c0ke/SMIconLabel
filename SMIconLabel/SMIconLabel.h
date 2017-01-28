a6450216ebeddede661e1534188787f42dced445
Jakkapong Yuangtong <visualizeq@gmail.com>
2017-01-28 23:38:29 +0700

    add carthage support

---
+++ SMIconLabel/SMIconLabel.h
@@ -0,0 +1,19 @@
+//
+//  SMIconLabel.h
+//  SMIconLabel
+//
+//  Created by Coke on 1/28/17.
+//  Copyright © 2017 Smartarium.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SMIconLabel.
+FOUNDATION_EXPORT double SMIconLabelVersionNumber;
+
+//! Project version string for SMIconLabel.
+FOUNDATION_EXPORT const unsigned char SMIconLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SMIconLabel/PublicHeader.h>
+
+
